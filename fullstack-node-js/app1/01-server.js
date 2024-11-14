a0c8ed9d9db0d8c96c2a28aadea2f14d1746ae01
szorfein <szorfein@protonmail.com>
2024-11-14 21:02:04 +0000

    fullstack node

---
+++ fullstack-node-js/app1/01-server.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const port = process.env.PORT || 1337
+
+const server = http.createServer(function (req, res) {
+  res.end('hi')
+})
+
+server.listen(port)
+console.log(`Listening on port ${port}`)
