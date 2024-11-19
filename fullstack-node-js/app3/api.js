1e6e6edacf52ed8552f11f87ea1b234398edd472
szorfein <szorfein@protonmail.com>
2024-11-19 19:45:46 +0000

    basic api

--- fullstack-node-js/app3/api.js
+++ fullstack-node-js/app3/api.js
@@ -2,6 +2,9 @@ const Products = require('./products')
 
 module.exports = {
   listProducts,
+  createProduct,
+  editProduct,
+  deleteProduct,
   getProduct
 }
 
@@ -17,6 +20,21 @@ async function listProducts(req, res) {
   } catch(err) {
     res.status(500).json({ error: err.message })
   }
+}
+
+async function createProduct(req, res) {
+  console.log('request body', req.body)
+  res.json(req.body)
+}
+
+async function editProduct(req, res) {
+  console.log('edit body', req.body)
+  res.json(req.body)
+}
+
+async function deleteProduct(req, res) {
+  console.log('delete body', req.body)
+  res.json(req.body)
 }
 
 async function getProduct(req, res, next) {
--- fullstack-node-js/app3/app.js
+++ fullstack-node-js/app3/app.js
@@ -3,14 +3,19 @@ express = require('express'),
 path = require('path'),
 fs = require('fs').promises,
 api = require('./api'),
-middleware = require('./middleware')
+middleware = require('./middleware'),
+bodyParser = require('body-parser')
 
 const
 app = express(),
 port = process.env.PORT || 1337
 
 app.use(middleware.cors)
+app.use(bodyParser.json())
 app.get('/products', api.listProducts)
+app.post('/products', api.createProduct)
+app.put('/product/:id', api.editProduct)
+app.delete('/product/:id', api.deleteProduct)
 app.get('/product/:id', api.getProduct)
 app.use(middleware.handleError)
 app.use(middleware.notFound)
